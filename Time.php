34cb387391d2f128ecffecb97fdac61ea5d4a2d3
Kyuma <xxkyumaxx@gmail.com>
2017-07-21 17:37:00 -0800

    Time code
    for change level time

---
+++ Time.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Time;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class Time extends PluginBase{
+
+public function onEnable(){
+$this->getLogger()->info("Plugin Enable");
+}
+
+public function onCommand(CommandSender $sender, Command $cmd, $lable, array $args){
+switch($cmd->getName()){
+case 'day':
+$sender->getLevel()->setTime(day);
+break;
+case 'night':
+$sender->getLevel()->setTime(night);
+  }
+ }
+}
