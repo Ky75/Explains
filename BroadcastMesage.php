7d644230bc7d2b9c85799c08fe830c7033b13e78
Kyuma <xxkyumaxx@gmail.com>
2017-07-21 17:48:34 -0800

    BroadCast Code
    for send message to all players in server

---
+++ BroadcastMesage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BroadcastMessage;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class BroadcastMessage extends PluginBase{
+
+public function onEnable(){
+$this->getLogger()->info("Plugin Enable");
+}
+
+public function onCommand(CommandSender $sender, Command $cmd, $lable, array $args){
+switch($cmd->getName()){
+case 'hi':
+$this->getServer()->sendbroadcastMessage("Hi");
+  }
+ }
+}
