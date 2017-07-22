37f5ab2a3a7a866f2a77592608221abf5fafec69
Kyuma <xxkyumaxx@gmail.com>
2017-07-22 11:38:41 -0800

    Update Time.php

--- Time.php
+++ Time.php
@@ -13,7 +13,7 @@ public function onEnable(){
 $this->getLogger()->info("Plugin Enable");
 }
 
-public function onCommand(CommandSender $sender, Command $cmd, $lable, array $args){
+public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 switch($cmd->getName()){
 case 'day':
 $sender->getLevel()->setTime(day);
