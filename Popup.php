d24615add57729cbbd9a6356469a231598c68c72
Kyuma <xxkyumaxx@gmail.com>
2017-07-22 20:29:03 -0800

    Popup
    For Send Message top Inventory

--- Popup.php
+++ Popup.php
@@ -15,8 +15,8 @@ $this->getLogger()->info("Plugin Enable")!
 
 public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 switch($cmd->getName()){
-case 'popup':
-$sender->sendPopup("Popup Message");
+ case 'popup':
+ $sender->sendPopup("Popup Message");
   }
  }
 }
