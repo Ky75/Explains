6d5f913845e66b43af9cd2f034ae8fdc7883ad1f
Kyuma <xxkyumaxx@gmail.com>
2017-07-24 06:48:17 +0300

    Flight
    for enable flight in survival

--- Flight.php
+++ Flight.php
@@ -5,18 +5,17 @@ namespace Flight;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
 
 class Flight extends PluginBase{
 
-public function onEnable(){
-$this->getLogger()->info("Plugin Enable");
-}
-
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-switch($cmd->getName()){
-case 'flight':
-$sender->setAllowFlight(true); //or false if you eant disable flight
+  public function onEnable(){
+    $this->getLogger()->info("Plugin Enable");
   }
- }
+
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+      case 'flight':
+        $sender->setAllowFlight(true); //or false if you eant disable flight
+    }
+  }
 }
