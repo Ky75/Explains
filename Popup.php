d6fb23731ed37bb6404acb108ea62a80f71cd228
Kyuma <xxkyumaxx@gmail.com>
2017-07-24 06:54:05 +0300

    Popup
    For Send Message top Inventory

--- Popup.php
+++ Popup.php
@@ -5,18 +5,17 @@ namespace Popup;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
 
 class Popup extends PluginBase{
 
-public function onEnable(){
-$this->getLogger()->info("Plugin Enable")!
-}
-
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-switch($cmd->getName()){
- case 'popup':
- $sender->sendPopup("Popup Message");
+  public function onEnable(){
+    $this->getLogger()->info("Plugin Enable")!
   }
- }
+
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+     case 'popup':
+       $sender->sendPopup("Popup Message");
+    }
+  }
 }
