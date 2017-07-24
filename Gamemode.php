0d39c48af98f2a4cf95a2c4ce84fa6e696b83018
Kyuma <xxkyumaxx@gmail.com>
2017-07-24 06:49:30 +0300

    Gamemode
    For Change player game mode

--- Gamemode.php
+++ Gamemode.php
@@ -5,19 +5,18 @@ namespace Gamemode;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
 
 class Gamemode extends PluginBase{
-//if plugin enable
-public function omEnable(){
-$this->getLogger()->info("Plugin Enable");
-}
-//comand 
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-switch($cmd->getName()){
- case 'gmc':
- $sender->setGamemode(1); //replace 1 to you want change gamemode
- $sender->sendMessage("Change Your Gamemode to 1");
+  //if plugin enable
+  public function omEnable(){
+    $this->getLogger()->info("Plugin Enable");
   }
- }
+  //command 
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+     case 'gmc':
+       $sender->setGamemode(1); //replace 1 to you want change gamemode
+       $sender->sendMessage("Change Your Gamemode to 1");
+    }
+  }
 }
