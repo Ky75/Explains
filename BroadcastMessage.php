3d5b92e216a760f60540c47e81947aab24faddb4
Kyuma <xxkyumaxx@gmail.com>
2017-07-24 06:46:13 +0300

    BroadcastMessage
    for send message to all players

--- BroadcastMessage.php
+++ BroadcastMessage.php
@@ -5,18 +5,17 @@ namespace BroadcastMessage;
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
-use pocketmine\Player;
 
 class BroadcastMessage extends PluginBase{
 
-public function onEnable(){
-$this->getLogger()->info("Plugin Enable");
-}
-
-public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
-switch($cmd->getName()){
- case 'hi':
- $this->getServer()->sendbroadcastMessage("Hi");
+  public function onEnable(){
+    $this->getLogger()->info("Plugin Enable");
   }
- }
+
+  public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+    switch($cmd->getName()){
+     case 'hi':
+     $this->getServer()->sendbroadcastMessage("Hi");
+    }
+  }
 }
