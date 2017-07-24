13880c5891b95407b3a038d95137450688ed340b
Kyuma <xxkyumaxx@gmail.com>
2017-07-23 17:16:51 -0800

    PlayerQuitEvent
    for  get player quit and send message or anything

---
+++ event/PlayerQuitEvent.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace event;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+
+class PlayerQuitEvent extends PluginBase implements Listener{
+
+public function onEnable(){
+$this->getServer()->getPluginManager()->registerEvents($this,$this);
+$this->getLogger()->info("Plugin Enable");
+}
+
+public function onQuit(PlayerQuitEvent $event){
+$player = $event->getPlayer();
+$name = $player->getName();
+$event->setQuitMessage($name."Quit Server");
+ }
+}
