1f24ca286033ebb36e4703c355986d64d4ba6f7f
Kyuma <xxkyumaxx@gmail.com>
2017-07-24 06:44:15 +0300

    PlayerJoinEvent
    for get player join to server

--- event/PlayerJoinEvent.php
+++ event/PlayerJoinEvent.php
@@ -6,19 +6,18 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\utils\TextFormat as Color;
-use pocketmine\Player;
 
 class PlayerJoinEvent extends PluginBase implements Listener{
 
-public function onEnable(){
-$this->getServer()->getPluginManager()->registerEvents($this,$this);
-$this->getLogger()->info("Plugin Enable");
-}
+  public function onEnable(){
+    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+    $this->getLogger()->info("Plugin Enable");
+  }
 
-public function onJoin(PlayerJoinEvent $event){
-$player = $event->getPlayer(); //for get player Join server
-$name = $player->getName(); //for get Player Name 
+  public function onJoin(PlayerJoinEvent $event){
+    $player = $event->getPlayer(); //for get player Join server
+    $name = $player->getName(); //for get Player Name 
 
-$player->sendMessage(TextFormat::YELLOW."Welcome".TextFormat::DARK_GRAY.$name); //for send Message Welcome Player_Name
+    $player->sendMessage(TextFormat::YELLOW."Welcome".TextFormat::DARK_GRAY.$name); //for send Message Welcome Player_Name
   }
 }
